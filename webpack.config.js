ff5c6f342b5c5878e046dc7b9c736552d268b651
abel-tefera <abeltefera16@gmail.com>
2023-03-28 10:14:20 +0300

    Webpack configured

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+/* eslint-disable */
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: { index: path.resolve(__dirname, "src", "index.js") },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src", "index.html"),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+    ]
+  },
+  mode: 'development'
+};
