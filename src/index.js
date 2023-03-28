1534371603aa1bff18a1581ba34bd5fc473cccf3
abel-tefera <abeltefera16@gmail.com>
2023-03-28 16:37:00 +0300

    Linters set up

--- src/index.js
+++ src/index.js
@@ -1,38 +1,36 @@
-import "./style.css";
+import './style.css';
 
 const todos = [
   {
     _id: 1,
-    title: "Work",
-    description: "You have to work to live?",
+    title: 'Work',
+    description: 'You have to work to live?',
     completed: false,
   },
   {
     _id: 2,
-    title: "Eat",
-    description: "You have to eat to live?",
+    title: 'Eat',
+    description: 'You have to eat to live?',
     completed: false,
   },
   {
     _id: 3,
-    title: "Sleep",
-    description: "You have to sleep to live?",
+    title: 'Sleep',
+    description: 'You have to sleep to live?',
     completed: false,
   },
 ];
 
 const main = () => {
-  const todoList = document.querySelector(".todo-list");
-  for (const [i, todo] of todos.entries()) {
-    const listItem = document.createElement("li");
-    // if (i < todos.length - 1) {
-      listItem.classList.add("todo-item");
-    // }
-    const check = document.createElement("input");
+  const todoList = document.querySelector('.todo-list');
+  for (const todo of todos) {
+    const listItem = document.createElement('li');
+    listItem.classList.add('todo-item');
+    const check = document.createElement('input');
     check.type = 'checkbox';
-    check.classList.add('todo-checkbox')
+    check.classList.add('todo-checkbox');
     listItem.innerHTML = `${todo.title}`;
-    listItem.insertAdjacentElement('afterbegin', check)
+    listItem.insertAdjacentElement('afterbegin', check);
     todoList.appendChild(listItem);
   }
 };
