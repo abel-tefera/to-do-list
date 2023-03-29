bf426678af02891ce2ab8233f2b553184fb46e2d
abel-tefera <abeltefera16@gmail.com>
2023-03-29 10:59:11 +0300

    Event handlers set up

--- src/index.js
+++ src/index.js
@@ -1,33 +1,21 @@
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
 import './style.css';
 import {TodoItem} from './modules/TodoItem';
+import {Todos} from './modules/Todos';
+import {Todo} from './modules/Todo';
 
-const todos = [
-  {
-    description: 'work out',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'complete To Do list project',
-    completed: false,
-    index: 2,
-  },
-  {
-    description: 'prepare for interview',
-    completed: true,
-    index: 3,
-  },
-];
+const todos = new Todos();
 
 customElements.define('todo-item', TodoItem);
+const todoList = document.querySelector('.todo-list');
 
 const main = () => {
-  const todoList = document.querySelector('.todo-list');
-  for (const todo of todos) {
+  for (const todo of todos.todoList) {
     const {index, description, completed} = todo;
 
     const todoLi = document.createElement('li');
-    todoLi.classList.add('todo-item', 'bottom-border');
+    todoLi.classList.add('bottom-border');
     todoLi.innerHTML = `<todo-item
     index=${JSON.stringify(index)}
     description=${JSON.stringify(description)}
@@ -36,6 +24,41 @@ const main = () => {
     ></todo-item>`;
     todoList.appendChild(todoLi);
   }
+
+  const checkBoxes = document.querySelectorAll('.todo-checkbox');
+  checkBoxes.forEach((checkbox) => {
+    checkbox.addEventListener('change', (e) => {
+      const index = e.target.id.split('-')[1];
+      todos.markCompleted(index);
+    });
+  });
 };
+const todoInput = document.querySelector('.todo-input');
+const clearCompleted = document.querySelector('.clear');
+
+todoInput.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter' || e.keyCode === 13) {
+    addTodoHandler();
+  }
+});
+
+const addTodoHandler = () => {
+  const currentLength = todos.todoList.length;
+  const todo = new Todo(todoInput.value, currentLength);
+  todos.addTodo(todo);
+  todoList.innerHTML = '';
+  todoInput.value = '';
+  main();
+};
+
+const removeHandler = () => {
+  todos.removeCompleted();
+  todoList.innerHTML = '';
+  main();
+};
+
+const addBtn = document.querySelector('.fa-plus');
+addBtn.addEventListener('click', addTodoHandler);
+clearCompleted.addEventListener('click', removeHandler);
 
 main();
