dbc0c6d1e7fd1a90efe0f38bf1db27187100434a
abel-tefera <abeltefera16@gmail.com>
2023-03-29 16:58:46 +0300

    Changes based on comments

--- src/index.js
+++ src/index.js
@@ -17,6 +17,7 @@ const main = () => {
     const todoLi = document.createElement('li');
     todoLi.classList.add('bottom-border');
     todoLi.innerHTML = `<todo-item
+    id="todo-item-${JSON.stringify(index)}"
     index=${JSON.stringify(index)}
     description=${JSON.stringify(description)}
     completed=${JSON.stringify(completed)}
@@ -24,12 +25,61 @@ const main = () => {
     ></todo-item>`;
     todoList.appendChild(todoLi);
   }
+  const handleEdit = (editId, newDesc) => {
+    todos.editTodo(editId, newDesc);
+    todoList.innerHTML = '';
+    main();
+  };
 
-  const checkBoxes = document.querySelectorAll('.todo-checkbox');
-  checkBoxes.forEach((checkbox) => {
-    checkbox.addEventListener('change', (e) => {
-      const index = e.target.id.split('-')[1];
-      todos.markCompleted(index);
+  const todoEdits = document.querySelectorAll('.todo-edit');
+  const todoTrash = document.querySelectorAll('.todo-trash');
+
+  todoTrash.forEach((trash) => {
+    trash.addEventListener('click', (e) => {
+      const index = e.target.id.split('-')[2];
+      todos.removeTodo(parseInt(index));
+      todoList.innerHTML = '';
+      main();
+    });
+    trash.style.display = 'none';
+  });
+
+  todoEdits.forEach((todoEdit) => {
+    todoEdit.addEventListener('keydown', (e) => {
+      if (e.key === 'Enter' || e.keyCode === 13) {
+        const editId = e.target.id.split('-')[2];
+        const newDesc = e.target.value;
+        handleEdit(editId, newDesc);
+      }
+    });
+    todoEdit.style.visibility = 'hidden';
+  });
+
+  const todoDescs = document.querySelectorAll('.todo-desc');
+  todoDescs.forEach((todoDesc) => {
+    todoDesc.addEventListener('click', (e) => {
+      const descId = e.target.id.split('-')[2];
+      todoDesc.style.visibility = 'hidden';
+      const todoMove = document.getElementById(`todo-move-${descId}`);
+      todoMove.style.display = 'none';
+
+      const editId = `todo-edit-${descId}`;
+      const spanId = `todo-span-${descId}`;
+      const containerId = `todo-item-${descId}`;
+      const trashId = `todo-delete-${descId}`;
+
+      const todoEdit = document.getElementById(editId);
+      const todoDelete = document.getElementById(trashId);
+      const todoSpan = document.getElementById(spanId);
+      todoSpan.classList.add('todo-span-reversed');
+
+      const todoContainer = document.getElementById(containerId);
+      todoContainer.style.backgroundColor = '#f1ea6e';
+
+      todoEdit.value = todoDesc.innerHTML.trim();
+      todoEdit.style.visibility = 'visible';
+      todoDelete.style.display = 'inline-block';
+      todoEdit.focus();
     });
   });
 };
@@ -52,7 +102,7 @@ const addTodoHandler = () => {
 };
 
 const removeHandler = () => {
-  todos.removeCompleted();
+  // todos.removeCompleted();
   todoList.innerHTML = '';
   main();
 };
